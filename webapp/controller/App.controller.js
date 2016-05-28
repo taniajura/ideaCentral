dcfee5d88827b4bb79c3164e9e9a82831feba9dc
taniajura <erick.taniajura@gmail.com>
2016-05-28 14:56:25 +0000

    Finally got the binding from json data file working! Refer to documentation on correct data path.
    https://sapui5.hana.ondemand.com/#docs/guide/91f0ed206f4d1014b6dd926db0e91070.html

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,22 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("com.ideacentral.controller.App", {
-
+		
+		onInit: function () {
+			//var oEmployeeModel = new sap.ui.model.json.JSONModel();
+			
+			//oEmployeeModel.loadData("./model/Employee.json");
+/*			oEmployeeModel.setData({
+			    firstName: "Peter",
+			    lastName: "Pan"
+			});*/
+			//sap.ui.getCore().setModel(oEmployeeModel, "employee");
+		}
+		
 	});
 
 });
